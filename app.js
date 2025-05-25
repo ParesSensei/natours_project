91559a62c0f0271c22cfc23d32a611223a4efa99
Pares <alfariz2004@gmail.com>
2025-05-25 16:57:08 +0700

    implement users route

--- app.js
+++ app.js
@@ -1,6 +1,12 @@
 const express = require('express');
 const fs = require('fs');
 const app = express();
+const morgan = require('morgan');
+
+
+// 1) MIDDLEWARE
+app.use(morgan('dev'));
+
 app.use(express.json());
 
 app.use((req, res, next) => {
@@ -16,6 +22,8 @@ app.use((req, res, next) => {
 const tours = JSON.parse(
     fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`)
 );
+
+// 2) ROUTE HANDLERS
 
 const getAllTours = (req, res) => {
     console.log(req.requestTime);
@@ -106,11 +114,47 @@ const deleteTour = (req, res) => {
     });
 };
 
-// app.get('/api/v1/tours', getAllTours);
-// app.post('/api/v1/tours', createTour);
-// app.get('/api/v1/tours/:id', getTour);
-// app.patch('/api/v1/tours/:id', updateTour);
-// app.delete('/api/v1/tours/:id', deleteTour);
+
+const getAllUsers = (req, res) => {
+    res.status(500).json({
+        status: 'error',
+        message: "This route is not yet defined"
+    });
+};
+
+
+const getUser = (req, res) => {
+    res.status(500).json({
+        status: 'error',
+        message: "This route is not yet defined"
+    });
+};
+
+
+const createUser = (req, res) => {
+    res.status(500).json({
+        status: 'error',
+        message: "This route is not yet defined"
+    });
+};
+
+
+const updateUser = (req, res) => {
+    res.status(500).json({
+        status: 'error',
+        message: "This route is not yet defined"
+    });
+};
+
+
+const deleteUser = (req, res) => {
+    res.status(500).json({
+        status: 'error',
+        message: "This route is not yet defined"
+    });
+};
+
+// 3) ROUTES
 
 app.route('/api/v1/tours')
     .get(getAllTours)
@@ -120,6 +164,17 @@ app.route('/api/v1/tours/:id')
     .get(getTour)
     .patch(updateTour)
     .delete(deleteTour);
+
+app.route('/api/v1/users')
+    .get(getAllUsers)
+    .post(createUser);
+
+app.route('/api/v1/users/:id')
+    .get(getUser)
+    .patch(updateUser)
+    .delete(deleteUser);;
+
+// 4) START SERVER
 
 const port = 3000;
 app.listen(port, () => {
